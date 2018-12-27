2e6096228467fea29bcf1605cfff044cc2e26c24
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2018-12-27 16:52:15 +0000

    Create useragent.js

---
+++ js/useragent.js
@@ -0,0 +1,11 @@
+
+/* TABLE 2: User Agent */
+
+document.getElementById("nUserAgent").innerHTML = navigator.userAgent;
+document.getElementById("nAppName").innerHTML = navigator.appName;
+document.getElementById("nCodeName").innerHTML = navigator.appCodeName;
+document.getElementById("nAppVersion").innerHTML = navigator.appVersion;
+document.getElementById("nPlatform").innerHTML = navigator.platform;
+document.getElementById("nOscpu").innerHTML = navigator.oscpu;
+document.getElementById("nOnLine").innerHTML = navigator.onLine;
+document.getElementById("nBuildID").innerHTML = navigator.buildID;
