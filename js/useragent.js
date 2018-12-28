a99d53f92d39a29d9f40c6dcd93ca8dee6d52f56
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2018-12-28 23:38:25 +0000

    move navigator.onLine

--- js/useragent.js
+++ js/useragent.js
@@ -5,7 +5,6 @@ document.getElementById("nAppName").innerHTML = navigator.appName;
 document.getElementById("nAppVersion").innerHTML = navigator.appVersion;
 document.getElementById("nBuildID").innerHTML = navigator.buildID;
 document.getElementById("nCodeName").innerHTML = navigator.appCodeName;
-document.getElementById("nOnLine").innerHTML = navigator.onLine;
 document.getElementById("nOscpu").innerHTML = navigator.oscpu;
 document.getElementById("nPlatform").innerHTML = navigator.platform;
 document.getElementById("nProduct").innerHTML = navigator.product;
