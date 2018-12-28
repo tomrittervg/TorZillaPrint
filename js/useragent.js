1f14d800a97cb3098dd4bd8cdcf5ef4b3a405f3b
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2018-12-28 21:55:07 +0000

    add product*, order ua js

--- js/useragent.js
+++ js/useragent.js
@@ -1,11 +1,13 @@
 
 /* TABLE 2: User Agent */
 
-document.getElementById("nUserAgent").innerHTML = navigator.userAgent;
 document.getElementById("nAppName").innerHTML = navigator.appName;
-document.getElementById("nCodeName").innerHTML = navigator.appCodeName;
 document.getElementById("nAppVersion").innerHTML = navigator.appVersion;
-document.getElementById("nPlatform").innerHTML = navigator.platform;
-document.getElementById("nOscpu").innerHTML = navigator.oscpu;
-document.getElementById("nOnLine").innerHTML = navigator.onLine;
 document.getElementById("nBuildID").innerHTML = navigator.buildID;
+document.getElementById("nCodeName").innerHTML = navigator.appCodeName;
+document.getElementById("nOnLine").innerHTML = navigator.onLine;
+document.getElementById("nOscpu").innerHTML = navigator.oscpu;
+document.getElementById("nPlatform").innerHTML = navigator.platform;
+document.getElementById("nProduct").innerHTML = navigator.product;
+document.getElementById("nProductSub").innerHTML = navigator.productSub;
+document.getElementById("nUserAgent").innerHTML = navigator.userAgent;
