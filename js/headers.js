eedc518a9bd4a699222ccd90ed76d877e02fde04
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2018-12-29 07:40:14 +0000

    Update headers.js

--- js/headers.js
+++ js/headers.js
@@ -1,5 +1,6 @@
 
 /* TABLE: HTTP Headers */
 
+document.getElementById("nConnection").innerHTML = navigator.connection;
 document.getElementById("nDoNotTrack").innerHTML = navigator.doNotTrack;
 document.getElementById("nOnLine").innerHTML = navigator.onLine;
