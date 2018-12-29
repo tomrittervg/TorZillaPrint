b0d4ed10b21ea5fc4f60ca346c48b052ff4aa956
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2018-12-29 05:47:06 +0000

    Update useragent.js

--- js/useragent.js
+++ js/useragent.js
@@ -1,6 +1,14 @@
 
 /* TABLE: User Agent */
 
+function checkImage(imageSrc, imageMsg) {
+    var img = new Image();
+    img.src = imageSrc;
+    img.onload = function(){ document.getElementById("IsFF").innerHTML = imageMsg};
+}
+
+checkImage("about:logo", "Firefox");
+
 document.getElementById("nAppName").innerHTML = navigator.appName;
 document.getElementById("nAppVersion").innerHTML = navigator.appVersion;
 document.getElementById("nBuildID").innerHTML = navigator.buildID;
@@ -11,4 +19,5 @@ document.getElementById("nProduct").innerHTML = navigator.product;
 document.getElementById("nProductSub").innerHTML = navigator.productSub;
 document.getElementById("nUserAgent").innerHTML = navigator.userAgent;
 
-function IsFirefox(){document.getElementById("IsFF").innerHTML = "Firefox"};
+// don't run immediately after about:logo image check
+checkImage("resource://normandy-content/about-studies/img/shield-logo.png", "Tor Browser");
