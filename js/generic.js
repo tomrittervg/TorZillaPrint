02b778a5f628066e7eb741bd011c228b8b317a36
claustromaniac <20734810+claustromaniac@users.noreply.github.com>
2018-12-30 15:11:36 -0300

    fix dpi, zoom, etc

--- js/generic.js
+++ js/generic.js
@@ -7,12 +7,15 @@ class DOM {
       if (e.id) this[e.id] = new Proxy(e, {
         get: function(obj, prop) {
           if (prop === 'val') return obj.textContent;
+          return obj[prop];
         },
         set: function(obj, prop, val) {
           if (prop === 'val') {
             obj.textContent = val;
             return true;
           }
+          obj[prop] = val;
+          return true;
         }
       });
     }
