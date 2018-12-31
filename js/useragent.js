e46f6ece6a6a7e60cc6d4f6b58ffe5e18d07ab6c
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2018-12-31 00:23:06 +0000

    fix chrome:// tor specific check

--- js/useragent.js
+++ js/useragent.js
@@ -20,5 +20,5 @@ dom.nProduct.val = navigator.product;
 dom.nProductSub.val = navigator.productSub;
 dom.nUserAgent.val = navigator.userAgent;
 
-// don't run immediately after about:logo image check
-checkImage("resource://normandy-content/about-studies/img/shield-logo.png", "Tor Browser");
+// run last due to timer delay
+setTimeout(checkImage("resource://normandy-content/about-studies/img/shield-logo.png", "Tor Browser"), 500);
