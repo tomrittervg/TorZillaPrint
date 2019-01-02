aa53497876a908d8ea7c2b281c311d66e88397fc
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-01-02 03:27:25 +0000

    add geo

--- js/language.js
+++ js/language.js
@@ -1,6 +1,11 @@
-/* TABLE:  Language & Locale / Time Zone & Date Format */
+/* TABLE:  Language & Locale / Time Zone & Date Format / Geolocation */
 
 'use strict';
 
+// language
 dom.nLanguage = navigator.language;
 dom.nLanguages = navigator.languages;
+
+// geo
+if ("geolocation" in navigator) {dom.nGeolocation="yes"} else (dom.nGeolocation="no");
+navigator.permissions.query({name:"geolocation"}).then(e => dom.pGeolocation=e.state);
