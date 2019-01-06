4ad8b6278b533887eed09298248acb7d5811a9ec
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-01-06 19:48:34 +0000

    add version detection

--- js/useragent.js
+++ js/useragent.js
@@ -31,3 +31,31 @@ dom.nPlatform = navigator.platform;
 dom.nProduct = navigator.product;
 dom.nProductSub = navigator.productSub;
 dom.nUserAgent = navigator.userAgent;
+
+/* Firefox Version 60+ Detection */
+function getVerNo(){
+  //59 or lower
+  var verNo = "59 or lower...";
+  //60
+  try {(Object.getOwnPropertyDescriptor(Document.prototype, "body")
+    || Object.getOwnPropertyDescriptor(HTMLDocument.prototype, "body")).get.call((new DOMParser).parseFromString(
+      "<html xmlns='http://www.w3.org/1999/xhtml'><body/></html>","application/xhtml+xml")) !== null; verNo="60";}
+  catch(err) {};
+  //61
+  var str61=" blah";
+  try {str61 = str61.trimStart(); verNo="61"} catch(err) {};
+  //62
+  console.time("ver62");
+  try {console.timeLog("ver62"); verNo="62"} catch(err) {};
+  console.timeEnd("ver62");
+  //63
+  if (Symbol.for(`foo`).description == "foo"){ verNo="63"};
+  //64
+  if (window.screenLeft == undefined){} else { verNo="64"};
+  //65
+  try {const rtf = new Intl.RelativeTimeFormat("en", {style: "long",}); verNo="65+"} catch(err) {};
+  // reminder: ^^ always append + ONLY on the LAST test
+  return verNo;
+  };
+// only run the function for Firefox
+if (isNaN(window.mozInnerScreenX) === false){dom.versionNo = getVerNo();}
