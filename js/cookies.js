06041de62c0082a1684ea864b866fcca3c14b3c9
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-01-12 16:44:34 +0000

    indexedDB

--- js/cookies.js
+++ js/cookies.js
@@ -9,76 +9,57 @@ function getCookie(cname) {
   var ca = decodedCookie.split(';');
   for(var i = 0; i <ca.length; i++) {
     var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
+    while (c.charAt(0) == ' ') {c = c.substring(1);}
+    if (c.indexOf(name) == 0) {return c.substring(name.length, c.length);}
   }
   return "";
 };
 
+// cookie supported
 dom.nCookieEnabled = navigator.cookieEnabled;
 
-// session cookie
+// cookie test
 var rndCname = Math.random().toString(36).substring(2, 15);
 var rndCvalue = Math.random().toString(36).substring(2, 15);
-//console.log("-----new test------");
-//console.log(" random name cookie: "+rndCname);
-//console.log("random value cookie: "+rndCvalue);
 document.cookie = rndCname+"="+rndCvalue;
 var cUser =  getCookie(rndCname);
-if (cUser != ""){
-  dom.cookieSession="yes";
-  //console.log("read cookie value: "+getCookie(rndCname) );
-  }
-else { dom.cookieSession="no" };
+if (cUser != ""){dom.cookieSession="yes"} else { dom.cookieSession="no"};
 
-// localStorage
+// localStorage test
 var rndSname = Math.random().toString(36).substring(2, 15);
 var rndSvalue = Math.random().toString(36).substring(2, 15);
-//console.log(" random name LS: "+rndSname);
-//console.log("random value LS: "+rndSvalue);
 var domLS = "";
-// setItem & getItem
-try {
-  localStorage.setItem(rndSname, rndSvalue);
-  //console.log("setItem LS: "+rndSname+" , "+rndSvalue);
-  // getItem
-  if(!localStorage.getItem(rndSname)) {domLS="no"}
-  else {
-    domLS="yes";
-    //console.log("getItem LS: "+ localStorage.getItem(rndSname));
-  };
-}
-catch(err) {domLS = "no";};
+try {localStorage.setItem(rndSname, rndSvalue);
+  if(!localStorage.getItem(rndSname)) {domLS="no"} else {domLS="yes"};}
+catch(err) {domLS = "no"};
 dom.storageLocalTest = domLS;
 
-// sessionStorage
+// sessionStorage test
 var rndSname = Math.random().toString(36).substring(2, 15);
 var rndSvalue = Math.random().toString(36).substring(2, 15);
-//console.log(" random name SS: "+rndSname);
-//console.log("random value SS: "+rndSvalue);
 var domSS = "";
-// setItem & getItem
-try {
-  sessionStorage.setItem(rndSname, rndSvalue);
-  //console.log("setItem SS: "+rndSname+" , "+rndSvalue);
-  // getItem
-  if(!sessionStorage.getItem(rndSname)) {domSS="no"}
-  else {
-    domSS="yes";
-    //console.log("getItem SS: "+ sessionStorage.getItem(rndSname));
-  };
-}
-catch(err) {domSS = "no";};
+try {sessionStorage.setItem(rndSname, rndSvalue);
+  if(!sessionStorage.getItem(rndSname)) {domSS="no"} else {domSS="yes"};}
+catch(err) {domSS = "no"};
 dom.storageSessionTest = domSS;
 
-// indexedDB
+// indexedDB supported
+try {if (!window.indexedDB) {dom.IDBSupported="no"} else {dom.IDBSupported="yes"};}
+catch(err) {dom.IDBSupported="no"};
+
+// indexedDB test
+var rndIDB = Math.random().toString(36).substring(2, 15);
+try {
+  var requestIDB = indexedDB.open(rndIDB);
+  requestIDB.onerror = function() {dom.IDBTest = "no"};
+  requestIDB.onsuccess = function() {
+    // success, now store some data and read it back
+    dom.IDBTest="yes, but more tests to come";
+    };}
+catch(err) {dom.IDBTest = "no"};
 
 // appCache: https://developer.mozilla.org/en-US/docs/Web/HTML/Using_the_application_cache
 // window.applicationCache.status == 2
 
-// workers
+// workers supported
 if (typeof(Worker) !== "undefined") {dom.workerSupported="yes"} else {dom.workerSupported="no"};
