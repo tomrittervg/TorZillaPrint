eef43426b42436296101e18269281bfd1fb3dd63
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-01-15 01:00:49 +0000

    Create devices.js

---
+++ js/devices.js
@@ -0,0 +1,6 @@
+/* TABLE: Devices & Hardware */
+
+'use strict';
+
+if ("clipboard" in navigator) {dom.nClipboard="enabled"} else {dom.nClipboard="disabled"};
+dom.nHardwareConcurrency = navigator.hardwareConcurrency;
