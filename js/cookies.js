91fd55e9079b60e43b7d1017bad15b1b9b951fb1
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-01-20 03:00:10 +0000

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -16,7 +16,8 @@ function getCookie(cname) {
 };
 
 // cookie support
-dom.nCookieEnabled = navigator.cookieEnabled;
+if (navigator.cookieEnabled == true) {dom.nCookieEnabled="enabled"}
+  else {dom.nCookieEnabled="disabled"};
 // cookie test
 var rndCname = Math.random().toString(36).substring(2, 15);
 var rndCvalue = Math.random().toString(36).substring(2, 15);
@@ -25,9 +26,9 @@ var cUser =  getCookie(rndCname);
 if (cUser != ""){dom.cookieTest="yes"} else { dom.cookieTest="no"};
 
 // localStorage support
-try {if (typeof(localStorage) != "undefined") {dom.storageLSupport="yes"}
-     else {dom.storageLSupport="no: undefined"};}
-catch(err) {dom.storageLSupport="no: catch(err)"};
+try {if (typeof(localStorage) != "undefined") {dom.storageLSupport="enabled"}
+     else {dom.storageLSupport="disabled: undefined"};}
+catch(err) {dom.storageLSupport="disabled: catch(err)"};
 // localStorage test
 var rndSname = Math.random().toString(36).substring(2, 15);
 var rndSvalue = Math.random().toString(36).substring(2, 15);
@@ -38,9 +39,9 @@ catch(err) {domLS = "no: catch(err)"};
 dom.storageLTest = domLS;
 
 // sessionStorage support
-try {if (typeof(sessionStorage) != "undefined") {dom.storageSSupport="yes"}
-     else {dom.storageSSupport="no: undefined"};}
-catch(err) {dom.storageSSupport="no: catch(err)"};
+try {if (typeof(sessionStorage) != "undefined") {dom.storageSSupport="enabled"}
+     else {dom.storageSSupport="disabled: undefined"};}
+catch(err) {dom.storageSSupport="disabled: catch(err)"};
 // sessionStorage test
 var rndSname = Math.random().toString(36).substring(2, 15);
 var rndSvalue = Math.random().toString(36).substring(2, 15);
@@ -51,8 +52,8 @@ catch(err) {domSS = "no: catch(err)"};
 dom.storageSTest = domSS;
 
 // indexedDB support
-try {if (!window.indexedDB) {dom.IDBSupport="no"} else {dom.IDBSupport="yes"};}
-catch(err) {dom.IDBSupport="no: catch(err)"};
+try {if (!window.indexedDB) {dom.IDBSupport="disabled"} else {dom.IDBSupport="enabled"};}
+catch(err) {dom.IDBSupport="disabled: catch(err)"};
 // indexedDB test
 var rndIDB = Math.random().toString(36).substring(2, 15);
 try {
@@ -60,15 +61,27 @@ try {
   requestIDB.onerror = function() {dom.IDBTest = "no: onerror"};
   requestIDB.onsuccess = function() {
     // success, now store some data and read it back
-    dom.IDBTest="yes, but more tests to come";
+    dom.IDBTest="yes: test to come";
     };}
 catch(err) {dom.IDBTest = "no: catch(err)"};
 
-// appCache: https://developer.mozilla.org/en-US/docs/Web/HTML/Using_the_application_cache
+// appCache support
+var appCS = "";
+if ("applicationCache" in window) {appCS="enabled"} else {appCS="disabled"};
+dom.appCacheSupport = appCS;
+// appCache test
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Using_the_application_cache
 // window.applicationCache.status == 2
+if (appCS == "enabled"){
+    dom.appCacheTest="yes: test to come"
+   }
+else {dom.appCacheTest="no"};
+
+// appCache permission
+navigator.permissions.query({name:"persistent-storage"}).then(e => dom.pPersistentStorage=e.state);
 
 // worker support
-if (typeof(Worker) !== "undefined") {dom.workerSupport="yes"} else {dom.workerSupport="no"};
+if (typeof(Worker) !== "undefined") {dom.workerSupport="enabled"} else {dom.workerSupport="disabled"};
 
 // web worker test
 var wwTest = "unknown"; var wwt;
@@ -99,21 +112,20 @@ if ((location.protocol) !== "file:") {
 // service worker support
 if ((location.protocol) !== "file:") {
   var swSupport = "";
-  if ('serviceWorker' in navigator) {swSupport="yes"} else {swSupport="no"};
+  if ('serviceWorker' in navigator) {swSupport="enabled"} else {swSupport="disabled"};
   dom.serviceWSupport = swSupport;
   // service worker test
-  if (swSupport == "yes"){
-      dom.serviceWTest="supported: test to come"
+  if (swSupport == "enabled"){
+      dom.serviceWTest="yes: test to come"
      }
   else {dom.serviceWTest="no"};
   // service worker cache
-  if (swSupport == "yes"){
-      dom.serviceWCache="supported: test to come"
+  if (swSupport == "enabled"){
+      dom.serviceWCache="yes: test to come"
      }
   else {dom.serviceWCache="no"};
 };
 
-// permissions
-navigator.permissions.query({name:"persistent-storage"}).then(e => dom.pPersistentStorage=e.state);
+// notifications / push
 navigator.permissions.query({name:"notifications"}).then(e => dom.pNotifications=e.state);
 navigator.permissions.query({name:"push"}).then(e => dom.pPush=e.state);
