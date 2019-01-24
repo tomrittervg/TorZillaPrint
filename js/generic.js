d60334672189dbd346790183cc5b3f4c2409e5a2
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-01-24 07:31:07 +0000

    add btyeArrayToHex

--- js/generic.js
+++ js/generic.js
@@ -13,14 +13,24 @@ function getUniqueElements() {
         return true;
     }
   });
-}
+};
 
 function base64Encode(str, encoding = 'utf-8') {
     var bytes = new (TextEncoder || TextEncoderLite)(encoding).encode(str);
     return base64js.fromByteArray(bytes);
-}
+};
 
 function base64Decode(str, encoding = 'utf-8') {
     var bytes = base64js.toByteArray(str);
     return new (TextDecoder || TextDecoderLite)(encoding).decode(bytes);
-}
+};
+
+function byteArrayToHex(arrayBuffer){
+  var chunks = [];
+  (new Uint32Array(arrayBuffer)).forEach(function(num){
+    chunks.push(num.toString(16));
+  });
+  return chunks.map(function(chunk){
+    return "0".repeat(8 - chunk.length) + chunk;
+  }).join("");
+};
