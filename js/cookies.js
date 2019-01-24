95225fb097666781e325a72472402c7d2c9242ab
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-01-24 09:30:58 +0000

    tidy up

--- js/cookies.js
+++ js/cookies.js
@@ -4,8 +4,7 @@
 
 // functions
 function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
+  var name = cname + "="; var decodedCookie = decodeURIComponent(document.cookie);
   var ca = decodedCookie.split(';');
   for(var i = 0; i <ca.length; i++) {
     var c = ca[i];
@@ -14,97 +13,81 @@ function getCookie(cname) {
   }
   return "";
 };
+function rndString() {return Math.random().toString(36).substring(2, 15);};
+var rndStr = "";
 
 // cookie support
-if (navigator.cookieEnabled == true) {dom.nCookieEnabled="enabled"}
-  else {dom.nCookieEnabled="disabled"};
+if (navigator.cookieEnabled == true) {dom.nCookieEnabled="enabled"} else {dom.nCookieEnabled="disabled"};
 // cookie test
-var rndCname = Math.random().toString(36).substring(2, 15);
-var rndCvalue = Math.random().toString(36).substring(2, 15);
-document.cookie = rndCname+"="+rndCvalue;
-var cUser =  getCookie(rndCname);
-if (cUser != ""){dom.cookieTest="yes"} else { dom.cookieTest="no"};
+rndStr = rndString(); document.cookie = rndStr+"="+rndStr;
+if (getCookie(rndStr) != ""){dom.cookieTest="yes"} else { dom.cookieTest="no"};
 
 // localStorage support
-try {if (typeof(localStorage) != "undefined") {dom.storageLSupport="enabled"}
-     else {dom.storageLSupport="disabled: undefined"};}
+try {
+  if (typeof(localStorage) != "undefined") {dom.storageLSupport="enabled";}
+  else {dom.storageLSupport="disabled: undefined"};}
 catch(err) {dom.storageLSupport="disabled: catch(err)"};
 // localStorage test
-var rndSname = Math.random().toString(36).substring(2, 15);
-var rndSvalue = Math.random().toString(36).substring(2, 15);
-var domLS = "";
-try {localStorage.setItem(rndSname, rndSvalue);
-  if(!localStorage.getItem(rndSname)) {domLS="no"} else {domLS="yes"};}
-catch(err) {domLS = "no: catch(err)"};
-dom.storageLTest = domLS;
+rndStr = rndString();
+try {localStorage.setItem(rndStr, rndStr);
+  if(!localStorage.getItem(rndStr)) {dom.storageLTest="no"} else {dom.storageLTest="yes"};}
+catch(err) {dom.storageLTest="no: catch(err)"};
 
 // sessionStorage support
-try {if (typeof(sessionStorage) != "undefined") {dom.storageSSupport="enabled"}
-     else {dom.storageSSupport="disabled: undefined"};}
+try {
+  if (typeof(sessionStorage) != "undefined") {dom.storageSSupport="enabled"}
+  else {dom.storageSSupport="disabled: undefined"};}
 catch(err) {dom.storageSSupport="disabled: catch(err)"};
 // sessionStorage test
-var rndSname = Math.random().toString(36).substring(2, 15);
-var rndSvalue = Math.random().toString(36).substring(2, 15);
-var domSS = "";
-try {sessionStorage.setItem(rndSname, rndSvalue);
-  if(!sessionStorage.getItem(rndSname)) {domSS="no"} else {domSS="yes"};}
-catch(err) {domSS = "no: catch(err)"};
-dom.storageSTest = domSS;
+rndStr = rndString();
+try {sessionStorage.setItem(rndStr, rndStr);
+  if(!sessionStorage.getItem(rndStr)) {dom.storageSTest="no"} else {dom.storageSTest="yes"};}
+catch(err) {dom.storageSTest="no: catch(err)"};
 
 // indexedDB support
 try {if (!window.indexedDB) {dom.IDBSupport="disabled"} else {dom.IDBSupport="enabled"};}
 catch(err) {dom.IDBSupport="disabled: catch(err)"};
 // indexedDB test
-var rndIDB = Math.random().toString(36).substring(2, 15);
+rndStr = rndString();
 try {
-  var requestIDB = indexedDB.open(rndIDB);
+  var requestIDB = indexedDB.open(rndStr);
   requestIDB.onerror = function() {dom.IDBTest = "no: onerror"};
   requestIDB.onsuccess = function() {
     // success, now store some data and read it back
     dom.IDBTest="yes: test to come";
     };}
-catch(err) {dom.IDBTest = "no: catch(err)"};
+catch(err) {dom.IDBTest="no: catch(err)"};
 
 // appCache support
-var appCS = "";
-if ("applicationCache" in window) {appCS="enabled"} else {appCS="disabled"};
-dom.appCacheSupport = appCS;
-// appCache test
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Using_the_application_cache
-// window.applicationCache.status == 2
-if (appCS == "enabled"){
-    dom.appCacheTest="yes: test to come"
-   }
-else {dom.appCacheTest="no"};
+if ("applicationCache" in window) {
+  dom.appCacheSupport="enabled";
+  // appCache test
+  dom.appCacheTest="yes: test to come";}
+else {dom.appCacheSupport="disabled"; dom.appCacheTest="no"};
 
 // worker support
-if (typeof(Worker) !== "undefined") {dom.workerSupport="enabled"} else {dom.workerSupport="disabled"};
-
-// web worker test
-var wwTest = "unknown"; var wwt;
 if (typeof(Worker) !== "undefined") {
-  wwt = new Worker("worker.js");
-  wwt.onmessage = wwTest="yes";
-  wwt.terminate();
-  }
-else {wwTest = "no"};
-dom.webWTest = wwTest;
-
-// shared worker test
-if ((location.protocol) !== "file:") {
-  var swTest = "unknown"; var swt;
-  if (typeof(Worker) !== "undefined") {
+  dom.workerSupport="enabled";
+  // web worker test
+  var wwt;
+  try {
+    wwt = new Worker("worker.js");
+    wwt.onmessage = dom.webWTest="yes";
+    wwt.terminate();}
+  catch(err) {dom.webWTest="no: catch(err)"};
+  // shared worker test
+  if ((location.protocol) !== "file:") {
+    var swt;
     try {
       swt = new SharedWorker("workershared.js");
       swt.port.start();
       swt.port.postMessage("are you there");
-      swt.port.onmessage = swTest="yes";
-    }
-    catch(err) {swTest = "no: catch(err)"};
-    }
-  else {swTest = "no"};
-  dom.sharedWTest = swTest;
-};
+      swt.port.onmessage = dom.sharedWTest="yes";}
+    catch(err) {dom.sharedWTest="no: catch(err)"};
+  }
+  else {dom.sharedWTest="no: file:/// not allowed"};
+}
+else {dom.workerSupport="disabled"; dom.webWTest="no"; dom.sharedWTest="no"};
 
 // service worker support
 if ((location.protocol) !== "file:") {
@@ -124,7 +107,7 @@ if ((location.protocol) !== "file:") {
   if (swSupport == "enabled"){
       dom.serviceWCacheSupport="sw supported but dom.caches.enabled not checked"
      }
-  else {dom.serviceWCacheSupport="no"};
+  else {dom.serviceWCacheSupport="disabled"};
   // test
   if (swSupport == "enabled"){
       dom.serviceWCacheTest="sw supported but dom.caches.enabled not checked"
@@ -144,10 +127,6 @@ dom.storageMSupport = smSupport;
 if ((location.protocol) !== "file:") {
   if (smSupport == "enabled") {
     try {
-      // this persistence test is slightly quirky
-      // 1. In FF it prompts, in TB it doesn't (no global pref)
-      // 2. In FF60+ if you allowed it, it remembers that (you need to clear site data to test for not persistent)
-      // 3. In TB it always returns not persistent
       navigator.storage.persist().then(function(persistent) {
         if (persistent) dom.storageMProp = "persistent";
         else dom.storageMProp = "not persistent";
