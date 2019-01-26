9f9fc4b74bf572de3794839f25b9ebc4104d26c6
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-01-26 08:54:27 +0000

    save some MOAR bytes

--- js/useragent.js
+++ js/useragent.js
@@ -12,20 +12,12 @@ dom.nProduct = navigator.product;
 dom.nProductSub = navigator.productSub;
 dom.nUserAgent = navigator.userAgent;
 
-// browser: resource
-(() => {
-  const FFImg = new Image();
-  //FFImg.src = "about:logo";
-  FFImg.src = "chrome://browser/content/aboutRobots-icon.png";
-  FFImg.onload = e => {
-    if (!dom.fdResource.textContent) dom.fdResource = "Firefox";
-  };
-  const TorImg = new Image();
-  TorImg.src = "resource://normandy-content/about-studies/img/shield-logo.png";
-  TorImg.onload = e => {
-    dom.fdResource = "Firefox";
-  };
-})();
+// browser: resource/chrome
+// could also use: resource://normandy-content/about-studies/img/shield-logo.png"
+// could also use: about:logo
+const FFImg = new Image();
+FFImg.src = "chrome://browser/content/aboutRobots-icon.png";
+FFImg.onload = e => {if (!dom.fdResource.textContent) dom.fdResource = "Firefox";};
 
 // browesr: feature detection
 if (isNaN(window.mozPaintCount) === false){ dom.fdPaintCount="Firefox"};
@@ -43,25 +35,25 @@ if ((Math.exp(xfd) - 1) == 1.7182818284590455) {let yfd; yfd = Math.exp(xfd);
 /* Firefox Version 60+ Detection */
 function getVerNo(){
   //59 or lower
-  var verNo = "59 or lower...";
+  var verNo = "59 or lower";
   //60
   try {(Object.getOwnPropertyDescriptor(Document.prototype, "body")
     || Object.getOwnPropertyDescriptor(HTMLDocument.prototype, "body")).get.call((new DOMParser).parseFromString(
       "<html xmlns='http://www.w3.org/1999/xhtml'><body/></html>","application/xhtml+xml")) !== null; verNo="60";}
-  catch(err) {};
+  catch(e) {};
   //61
   var str61=" blah";
-  try {str61 = str61.trimStart(); verNo="61"} catch(err) {};
+  try {str61 = str61.trimStart(); verNo="61"} catch(e) {};
   //62
   console.time("ver62");
-  try {console.timeLog("ver62"); verNo="62"} catch(err) {};
+  try {console.timeLog("ver62"); verNo="62"} catch(e) {};
   console.timeEnd("ver62");
   //63
   if (Symbol.for(`foo`).description == "foo"){ verNo="63"};
   //64
   if (window.screenLeft == undefined){} else { verNo="64"};
   //65
-  try {const rtf = new Intl.RelativeTimeFormat("en", {style: "long",}); verNo="65+"} catch(err) {};
+  try {const rtf = new Intl.RelativeTimeFormat("en", {style: "long",}); verNo="65+"} catch(e) {};
   // reminder: ^^ always append + ONLY on the LAST test
   return verNo;
   };
