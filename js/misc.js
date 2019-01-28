df269b109819ee7128efa7b4fdfca74b88a93b82
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-01-28 22:06:13 +0000

    Update misc.js

--- js/misc.js
+++ js/misc.js
@@ -2,4 +2,15 @@
 
 'use strict';
 
+/* clipboard */
 if ("clipboard" in navigator) {dom.nClipboard="enabled"} else {dom.nClipboard="disabled"};
+
+/* Intersection Observer api (dom.IntersectionObserver.enabled) */
+var callback = function(entries, observer) {};
+try {
+  var observer = new IntersectionObserver(callback);
+  dom.intObserver="enabled"
+} catch(e) {dom.intObserver="disabled" };
+
+/* requestIdleCallback (dom.requestIdleCallback.enabled) */
+if ("requestIdleCallback" in window) {dom.reqIdleCB="enabled"} else {dom.reqIdleCB="disabled"};
