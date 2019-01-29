2e58aebd3e55cbcedd4afba3f6b5964133e5273e
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-01-29 05:27:43 +0000

    alrighty!
    modify the value to send back with a prefix

--- js/workershared.js
+++ js/workershared.js
@@ -2,14 +2,12 @@
 
 'use strict';
 
-var ports = [] ;
-
+var ports = [];
 onconnect = function(e) {
   var port = e.ports[0];
   ports.push(port);
   port.start();
   port.onmessage = function(e) {
-    port.postMessage(e.data);
-    console.log("shared worker data recieved from main thread: (onmessage) " + e.data);
+    port.postMessage("TZP-"+e.data);
   };
 };
