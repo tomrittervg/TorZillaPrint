ca2331e2d3fdaa4b542fe2260748f2b59d971a1b
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-01-29 03:53:42 +0000

    Update worker.js

--- js/worker.js
+++ js/worker.js
@@ -2,6 +2,12 @@
 
 'use strict';
 
-function testWWorker() {
-  postMessage("hi i'm a worker");
+self.addEventListener('message', function(e) => {
+  console.log("web worker data recieved from main thread: (event) " + e.data);
+  self.postMessage(e.data);
+}, false);
+
+this.onmessage = function() {
+  console.log("web worker data recieved from main thread: (onmessage) " + e.data);
+  postMessage(e.data);
 };
