7c8aeba3ab9e714b93a8b8ebb11facc48137c1fe
Thorin-Oakenpants <Thorin-Oakenpants@users.noreply.github.com>
2019-01-29 03:54:50 +0000

    Update workershared.js

--- js/workershared.js
+++ js/workershared.js
@@ -1,8 +1,15 @@
 /* SHARED WORKER SCRIPT */
 
+'use strict';
+
+var ports = [] ;
+
 onconnect = function(e) {
   var port = e.ports[0];
+  ports.push(port);
+  port.start();
   port.onmessage = function(e) {
-    port.postMessage("hi i'm a shared worker");
-  }
-}
+    port.postMessage(e.data);
+    console.log("shared worker data recieved from main thread: (onmessage) " + e.data);
+  };
+};
